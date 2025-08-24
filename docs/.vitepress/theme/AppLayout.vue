2191373c84706b39f2aa5202117ffa62a795a5b2
Kishan Sunar <mailkishansunar@gmail.com>
2025-08-24 18:36:13 +0545

    feat(components): add HeroSection component and integrate it into AppLayout; enhance styles with new CSS variables and utility classes

--- docs/.vitepress/theme/AppLayout.vue
+++ docs/.vitepress/theme/AppLayout.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
+import HeroSection from '../../src/components/HeroSection.vue'
 
 const { Layout } = DefaultTheme
 </script>
@@ -8,6 +9,10 @@ const { Layout } = DefaultTheme
   <Layout>
     <template #aside-outline-before>
       My custom sidebar top content
+    </template>
+
+    <template #home-hero-image>
+      <HeroSection />
     </template>
   </Layout>
 </template>
